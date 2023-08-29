5129dc3093a51a71bd0ebaaadf1c2fabfeb1b06e
JWu <johnnyw131@gmail.com>
2023-08-29 02:20:00 -0400

    I can view a list of all my pokemon in the browswer

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,7 +1,8 @@
 const React = require('react')
 
 function Index(props){
-    // const {fruits} = props
+     const {pokemon} = props
+     console.log(pokemon)
     const myStyle = {
         color: '#ffffff',
         backgroundColor: '#000000',
@@ -10,7 +11,16 @@ function Index(props){
         <main>
             
             <h1 style={myStyle}>Sell All the Pokemon!</h1>
-            
+            <ul>
+                {pokemon.map((monster,i) => {
+                    return(
+                        <li key={i}>
+                            The
+                            <a href={`/pokemon/${i}`} style={{textTransform: 'capitalize'}}> {monster.name}</a> 
+                        </li>
+                    )
+                })}
+            </ul>
         </main>
     )
 }
