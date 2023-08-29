774000a12419ce3786c777b7664613acf5927f64
JWu <johnnyw131@gmail.com>
2023-08-29 02:00:43 -0400

    connected my database can see json in the browser

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const jsxEngine = require('jsx-view-engine')
+const {pokemon} = require('./models/pokemon')
 
 
 // App Config
@@ -22,6 +23,14 @@ app.use(express.urlencoded({extended:false})); //middleware
 app.get('/', (req, res) => {
     res.send('Welcome to the Pokemon App!')
 })
+
+app.get('/pokemon', (req, res) => {
+    res.send(pokemon)
+})
+
+
+
+
 
 app.listen(port, () => {
     console.log('listening');
